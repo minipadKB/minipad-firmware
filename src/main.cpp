1b7eb13e6c2246aa3edfebe3ab953333d9cde9cc
minisbett <39670899+minisbett@users.noreply.github.com>
2023-02-06 17:02:26 +0100

    Improve documentation

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 #include "handlers/keypad_handler.hpp"
 #include "definitions.hpp"
 
-// The version of this firmware in the YYYYMMDD.PATCH format .(e.g. 20221219.2 for the 2nd release on the 19th december 2022)
+// The version of this firmware in the YYYYMMDD.PATCH format. (e.g. 20221219.2 for the 2nd release on the 19th december 2022)
 #ifdef KEYS_3
 char *firmwareVersion = (char *)"20221219.2-3k";
 #else
 char *firmwareVersion = (char *)"20221219.2-2k";
 #endif
 
-// Default configuration file loaded into the EEPROM if no configuration was set yet. Also used later on to reset the keypad and calibration
-// structs that might get modified on a firmware update and have to be reset back to their default values then
+// Default configuration file loaded into the EEPROM if no configuration was saved yet. Also used to reset the keypad and calibration
+// structs that might get modified on a firmware update and have to be reset back to their default values then later on.
 Configuration defaultConfig =
 {
   .version = Configuration::getVersion(),
@@ -67,13 +67,11 @@ KeypadHandler keypadHandler = KeypadHandler(&configController, &tolerances);
 
 void setup()
 {
-    // Initialize the serial interface.
-    Serial.begin(115200);
-
     // Load the configuration from the EEPROM.
     configController.loadConfig();
 
     // Initialize the serial and HID interface.
+    Serial.begin(115200);
     Keyboard.begin();
 
     // Set the reference voltage for the ADC to 2.56V for a better resolution on the analog readings.
