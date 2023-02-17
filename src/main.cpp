0d8afa2b927fed58fde41f0e23ee38d1c8c8543b
minisbett <39670899+minisbett@users.noreply.github.com>
2023-02-17 15:55:04 +0100

    Add prebuild script

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Import all needed libraries and headers
 #include <Arduino.h>
 #include <EEPROM.h>
+//#include <Keyboard.h>
 #include "config/configuration_controller.hpp"
 #include "handlers/serial_handler.hpp"
 #include "handlers/keypad_handler.hpp"
