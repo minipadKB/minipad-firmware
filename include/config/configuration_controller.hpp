2bb9480cdc218bd22b4b2e1f757a2618bf12d500
minisbett <39670899+minisbett@users.noreply.github.com>
2023-04-29 15:16:10 +0200

    Move getDefaultConfig into configuration controller

--- include/config/configuration_controller.hpp
+++ include/config/configuration_controller.hpp
@@ -3,55 +3,6 @@
 #include "config/configuration.hpp"
 #include "definitions.hpp"
 
-// Default configuration loaded into the EEPROM if no configuration was saved yet. Also used to reset the keypad and calibration
-// structs that might get modified on a firmware update and have to be reset back to their default values then later on.
-Configuration getDefaultConfig()
-{
-    Configuration config = {
-        .name = {'m', 'i', 'n', 'i', 'p', 'a', 'd'},
-        .heKeys = {},
-        .digitalKeys = {}
-    };
-
-    // Populate the hall effect keys array with the correct amount of hall effect keys.
-    for(uint8_t i = 0; i < HE_KEYS; i++)
-    {
-        config.heKeys[i] = {
-            .index = i,
-            .rapidTrigger = false,
-            .continuousRapidTrigger = false,
-
-            // For the default values of these values, a value depending on the total travel distance is being used.
-            .rapidTriggerUpSensitivity = TRAVEL_DISTANCE_IN_0_01MM / 10,
-            .rapidTriggerDownSensitivity = TRAVEL_DISTANCE_IN_0_01MM / 10,
-            .lowerHysteresis = (uint16_t)(TRAVEL_DISTANCE_IN_0_01MM * 0.55),
-            .upperHysteresis = (uint16_t)(TRAVEL_DISTANCE_IN_0_01MM * 0.675),
-
-             // Assign the keys from z downwards. (z, y, x, w, v, ...)
-             // With too many keys (>26?), this would eventually run out of characters.
-            .keyChar = (char)('z' - i),
-            .hidEnabled = false,
-            .restPosition = 1800,
-            .downPosition = 1100
-        };
-    }
-
-    // Populate the digital keys array with the correct amount of digital keys.
-    for(uint8_t i = 0; i < DIGITAL_KEYS; i++)
-    {
-        config.digitalKeys[i] = {
-            .index = i,
-
-             // Assign the keys from a forwards. (z, y, x, w, v, ...)
-             // With too many keys (>26?), this would eventually run out of characters.
-            .keyChar = (char)('a' + i),
-            .hidEnabled = false
-        };
-    }
-
-    return config;
-};
-
 inline class ConfigurationController
 {
 public:
@@ -68,4 +19,50 @@ public:
 
 private:
     Configuration defaultConfig;
+
+    // Default configuration loaded into the EEPROM if no configuration was saved yet. Also used to reset the keypad and calibration
+    // structs that might get modified on a firmware update and have to be reset back to their default values then later on.
+    Configuration getDefaultConfig()
+    {
+        Configuration config = {
+            .name = {'m', 'i', 'n', 'i', 'p', 'a', 'd'},
+            .heKeys = {},
+            .digitalKeys = {}};
+
+        // Populate the hall effect keys array with the correct amount of hall effect keys.
+        for (uint8_t i = 0; i < HE_KEYS; i++)
+        {
+            config.heKeys[i] = {
+                .index = i,
+                .rapidTrigger = false,
+                .continuousRapidTrigger = false,
+
+                // For the default values of these values, a value depending on the total travel distance is being used.
+                .rapidTriggerUpSensitivity = TRAVEL_DISTANCE_IN_0_01MM / 10,
+                .rapidTriggerDownSensitivity = TRAVEL_DISTANCE_IN_0_01MM / 10,
+                .lowerHysteresis = (uint16_t)(TRAVEL_DISTANCE_IN_0_01MM * 0.55),
+                .upperHysteresis = (uint16_t)(TRAVEL_DISTANCE_IN_0_01MM * 0.675),
+
+                // Assign the keys from z downwards. (z, y, x, w, v, ...)
+                // With too many keys (>26?), this would eventually run out of characters.
+                .keyChar = (char)('z' - i),
+                .hidEnabled = false,
+                .restPosition = 1800,
+                .downPosition = 1100};
+        }
+
+        // Populate the digital keys array with the correct amount of digital keys.
+        for (uint8_t i = 0; i < DIGITAL_KEYS; i++)
+        {
+            config.digitalKeys[i] = {
+                .index = i,
+
+                // Assign the keys from a forwards. (z, y, x, w, v, ...)
+                // With too many keys (>26?), this would eventually run out of characters.
+                .keyChar = (char)('a' + i),
+                .hidEnabled = false};
+        }
+
+        return config;
+    };
 } ConfigController;
